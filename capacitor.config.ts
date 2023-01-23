e61f9d6a5b887bd495dfbd7d67857b2f063e09f5
Deven Goratela <deven.goratela@tcs.com>
2023-01-23 18:50:59 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'amplify-capability-poc-mobile',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
